d830bb48741bca4adbac569f2cdc8181ff025fb4
Juncal González <jjuncal@indra.es>
2018-01-31 11:00:22 +0100

    Diseño inicial de la primera ventana de las operaciones

--- src/es/com/cc/ui/EdicionXmlPanel.java
+++ src/es/com/cc/ui/EdicionXmlPanel.java
@@ -9,8 +9,15 @@ import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableColumnModel;
 import javax.swing.border.LineBorder;
 import java.awt.Color;
+import java.awt.Dimension;
+
 import javax.swing.JScrollPane;
 import javax.swing.ListSelectionModel;
+import javax.swing.SwingConstants;
+import java.awt.GridLayout;
+import java.awt.FlowLayout;
+import javax.swing.JTextField;
+import java.awt.BorderLayout;
 
 public class EdicionXmlPanel extends JPanel {
 
@@ -18,13 +25,22 @@ public class EdicionXmlPanel extends JPanel {
 	
 	private static PrincipalPanel frame;
 	
-	private static String[] columnas = new String[] {"Tipo, Valor1, Valor2, Valor3" };
+	private static String[] columnas = new String[] {
+			"Tipo", "Valor1", "Valor2", "Valor3" };
 	private static Object[][] valores = new Object[][] {
-		{"Cancelacion", "valor1", "valor2", "valor3"},
-		{"Nueva", "valor1", "valor2", "valor3"}
+			{"Cancelacion", "valor1", "valor2", "valor3"},
+			{"Nueva",       "valor1", "valor2", "valor3"}
 	};
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private JTextField textField_5;
+	private JTextField textField_6;
 	private JTable table;
-	private JTable table_1;
+	
+	
 
 	/**
 	 * Create the panel.
@@ -32,45 +48,101 @@ public class EdicionXmlPanel extends JPanel {
 	public EdicionXmlPanel(PrincipalPanel frame) {
 		EdicionXmlPanel.frame = frame;
 		
-		setBounds(0, 0, 580, 831);
-		setLayout(null);
+		setBounds(0, 0, 729, 831);
+		setLayout(new BorderLayout(0, 0));
 		
-		JButton btnGuardar = new JButton("Save");
-		btnGuardar.setBounds(102, 11, 89, 23);
-		add(btnGuardar);
+		JPanel panel1 = new JPanel();
+		panel1.setPreferredSize(new Dimension(580, 200));
+		panel1.setLayout(null);
+		add(panel1, BorderLayout.NORTH);
 		
-		JButton btnLoad = new JButton("Load");
-		btnLoad.setBounds(239, 11, 89, 23);
-		add(btnLoad);
+		JLabel lblNewLabel = new JLabel("Id");
+		lblNewLabel.setBounds(54, 36, 46, 14);
+		panel1.add(lblNewLabel);
 		
-		JButton btnNewButton = new JButton("Show XML");
-		btnNewButton.setBounds(348, 11, 115, 23);
-		add(btnNewButton);
+		textField = new JTextField();
+		textField.setBounds(110, 33, 200, 20);
+		panel1.add(textField);
+		textField.setColumns(10);
 		
-		JLabel lblNewLabel = new JLabel("Transacciones");
-		lblNewLabel.setBounds(38, 201, 393, 14);
-		add(lblNewLabel);
+		JLabel lblNewLabel_1 = new JLabel("Prtry");
+		lblNewLabel_1.setBounds(54, 61, 46, 14);
+		panel1.add(lblNewLabel_1);
 		
-		table_1 = new JTable();
-		table_1.setBackground(Color.ORANGE);
-		table_1.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
-		table_1.setModel(new DefaultTableModel(
-			new Object[][] {
-			},
-			new String[] {
-				"Valor3", "Valor2", "Valor1", "Tipo"
-			}
-		) {
-			Class[] columnTypes = new Class[] {
-				Object.class, Object.class, Object.class, String.class
-			};
-			public Class getColumnClass(int columnIndex) {
-				return columnTypes[columnIndex];
-			}
-		});
-		table_1.setBorder(new LineBorder(Color.RED, 3, true));
-		table_1.setBounds(38, 385, 425, -116);
-		add(table_1);
+		textField_1 = new JTextField();
+		textField_1.setBounds(110, 58, 200, 20);
+		panel1.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblNewLabel_2 = new JLabel("FROM");
+		lblNewLabel_2.setBounds(10, 11, 46, 14);
+		panel1.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("TO");
+		lblNewLabel_3.setBounds(356, 11, 46, 14);
+		panel1.add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_4 = new JLabel("Id");
+		lblNewLabel_4.setBounds(384, 36, 46, 14);
+		panel1.add(lblNewLabel_4);
+		
+		JLabel lblNewLabel_5 = new JLabel("Prtry");
+		lblNewLabel_5.setBounds(384, 61, 46, 14);
+		panel1.add(lblNewLabel_5);
+		
+		textField_2 = new JTextField();
+		textField_2.setBounds(440, 33, 200, 20);
+		panel1.add(textField_2);
+		textField_2.setColumns(10);
+		
+		textField_3 = new JTextField();
+		textField_3.setBounds(440, 58, 200, 20);
+		panel1.add(textField_3);
+		textField_3.setColumns(10);
+		
+		JLabel lblNewLabel_6 = new JLabel("BizMsgIdr");
+		lblNewLabel_6.setBounds(10, 108, 69, 14);
+		panel1.add(lblNewLabel_6);
+		
+		JLabel lblNewLabel_7 = new JLabel("MsgDefIdr");
+		lblNewLabel_7.setBounds(10, 133, 69, 14);
+		panel1.add(lblNewLabel_7);
+		
+		JLabel lblNewLabel_8 = new JLabel("CreDt");
+		lblNewLabel_8.setBounds(10, 158, 69, 14);
+		panel1.add(lblNewLabel_8);
+		
+		textField_4 = new JTextField();
+		textField_4.setBounds(89, 105, 200, 20);
+		panel1.add(textField_4);
+		textField_4.setColumns(10);
+		
+		textField_5 = new JTextField();
+		textField_5.setBounds(89, 130, 200, 20);
+		panel1.add(textField_5);
+		textField_5.setColumns(10);
+		
+		textField_6 = new JTextField();
+		textField_6.setBounds(89, 155, 200, 20);
+		panel1.add(textField_6);
+		textField_6.setColumns(10);
+		
+		JPanel panel2 = new JPanel();
+		panel2.setPreferredSize(new Dimension(580, 400));
+		add(panel2, BorderLayout.CENTER);
+		
+		
+		table = new JTable(valores, columnas);
+		table.setPreferredSize(new Dimension(580, 400));
+		table.getTableHeader().setVisible(true);
+		panel2.add(table);
+		
+		JScrollPane scrollPane = new JScrollPane(table);
+		scrollPane.setPreferredSize(new Dimension(580, 400));
+		panel2.add(scrollPane);
+		
+		
+		
 		
 		
 	}
